e1a53b6307c734f98f51111be1097838ed5acddd
Turkay Kurklu <turkay@example.com>
2026-04-08 10:41:10 +0300

    Prisma Fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,12 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+// ✅ BURASI KRİTİK
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined ❌");
+}
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
@@ -15,7 +20,12 @@ export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
